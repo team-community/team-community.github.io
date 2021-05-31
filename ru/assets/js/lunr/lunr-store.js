4337e853568895e8b27664074cd49fa1423408a8
dev <user@users.noreply.github.com>
2021-05-31 10:19:02 +0300

    8

--- ru/assets/js/lunr/lunr-store.js
+++ ru/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Скачать ТимРэдМайнер 0.8.3 - AMD+NVIDIA GPU Miner [2021]",
-        "excerpt":"  Начните добывать Ethereum  \t\t \t\t\t \t\tВерсия: 5.6d  \t\tФайл: *PhoenixMiner.exe,   \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Скачать для Linux)\t \t\t \t\t \t\t \t\t \t\tСкачать сейчас(Windows x64) \t\t \t\t(зеркало) \t\t \t     Вышло обновление Феникс Майнера. Вы можете скачать TeamRedMiner 0.8.3 отсюда (настроенная и готовая к запуску версия для Windows и Linux.   Скачать и установить   Скачать Сейчас   или:   (зеркало)   Версия для Linux   Обязательно замените пул и адрес кошелька тем, что вы используете во всех файлах.   ","categories": ["download"],
+        "excerpt":"  Начните добывать Ethereum  \t\t \t\t\t \t\tВерсия: 0.8.3  \t\tФайл: *TeamRedMiner.exe,   \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Скачать для Linux)\t \t\t \t\t \t\t \t\t \t\tСкачать сейчас(Windows x64) \t\t \t\t(зеркало) \t\t \t     Вышло обновление Феникс Майнера. Вы можете скачать TeamRedMiner 0.8.3 отсюда (настроенная и готовая к запуску версия для Windows и Linux.   Скачать и установить   Скачать Сейчас   или:   (зеркало)   Версия для Linux   Обязательно замените пул и адрес кошелька тем, что вы используете во всех файлах.   ","categories": ["download"],
         "tags": ["TeamRedMiner"],
         "url": "https://teamredminer.info/ru/download/5.6d/",
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
