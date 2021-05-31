f2f42d17ba7c4aad378c064f94cdac2f56d57b1c
dev <user@users.noreply.github.com>
2021-05-31 10:00:06 +0300

    8

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
       },{
         "title": "Download TeamRedMiner  0.8.3 - AMD+NVIDIA GPU Miner [2021]",
-        "excerpt":"  Getting Started With Ethereum Mining  \t\t \t\t\t \t\tVersion: 5.6d  \t\tFile: *PhoenixMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new TeamRedMiner stale version is ready.   You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   The file contains the link to download the .zip archive and the password (phoenix) to unlock it.   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "excerpt":"  Getting Started With Ethereum Mining  \t\t \t\t\t \t\tVersion: 5.6d  \t\tFile: *PhoenixMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new TeamRedMiner stale version is ready.   You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["TeamRedMiner"],
         "url": "https://teamredminer.info/download/0.8.3/",
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
