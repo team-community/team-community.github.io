cbe498e11e2fc7834c2cbfd500c1863942d8d568
dev <user@users.noreply.github.com>
2021-06-05 14:08:12 +0300

    photo

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -18,7 +18,7 @@ var store = [{
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
       },{
         "title": "Download TeamRedMiner  0.8.3 - AMD GPU Miner [2021]",
-        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 0.8.3  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new TeamRedMiner stale version is ready.   You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 0.8.3  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t       You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["TeamRedMiner"],
         "url": "https://teamredminer.info/download/0.8.3/",
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
--- es/assets/js/lunr/lunr-store.js
+++ es/assets/js/lunr/lunr-store.js
@@ -5,8 +5,8 @@ var store = [{
         "url": "https://teamredminer.info/documentation/setup/",
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
       },{
-        "title": "Download TeamRedMiner 0.8.3 - AMD GPU Miner [2021]",
-        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 0.8.3  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t    The new TeamRedMiner beta version with ETCHash support is ready.    You can download TeamRedMiner from here:   Download Now   or:   Mega (mirror)   Inside the archive you will find a file README.txt with installation instructions.   ","categories": ["download"],
+        "title": "Download TeamRedMiner  0.8.3 - AMD GPU Miner [2021]",
+        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 0.8.3  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t       You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["TeamRedMiner"],
         "url": "https://teamredminer.info/download/0.8.3/",
         "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
