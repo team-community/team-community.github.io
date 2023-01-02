4c7cd5a4e30805e95baf7ee1bf1f3101241dcdd8
Your Name <you@example.com>
2023-01-02 12:05:57 +0300

    sass

---
+++ es/assets/js/lunr/lunr-store.js
@@ -0,0 +1,17 @@
+var store = [{
+        "title": "Argumentos de la línea de comandos de TeamRedMiner [2021]",
+<<<<<<< HEAD
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by TeamRedMiner.    You can download TeamRedMiner 0.10.7 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace TeamRedMiner.exe with ./TeamRedMiner in the command-line examples below.   ","categories": ["documentation"],
+=======
+        "excerpt":"You can use this list as a reference for all Launch Parameters supported by TeamRedMiner.    You can download TeamRedMiner 0.10.5.1 from here:   Download Now   Download (mirror)   Linux:  Under Linux you need to replace TeamRedMiner.exe with ./TeamRedMiner in the command-line examples below.   ","categories": ["documentation"],
+>>>>>>> 390176fe6a2c496c46b7fa2c0a50edc93d84d9fa
+        "tags": ["TeamRedMiner"],
+        "url": "https://teamredminer.info/documentation/setup/",
+        "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
+      },{
+        "title": "Download TeamRedMiner  0.8.3 - AMD GPU Miner [2021]",
+        "excerpt":" Get started with TeamRedMiner  \t\t \t\t\t \t\tVersion: 0.8.3  \t\tFile: *TeamRedMiner.exe,    \t\tSHA256: e8e775add50c67e1c6f6ca20db318f745e22b085afcbdf5634015e6ef91e8853  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t       You can download TeamRedMiner 0.8.3 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "tags": ["TeamRedMiner"],
+        "url": "https://teamredminer.info/download/0.8.3/",
+        "teaser": "https://teamredminer.info/assets/images/thumbs/500x300.png"
+      },]
